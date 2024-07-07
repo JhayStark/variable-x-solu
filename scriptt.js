85dbf319d4d50c4e092bc0f115a479bd25c4833e
PrissyGIT <gitpriscilla@gmail.com>
2024-07-07 09:45:59 +0000

    Add files via upload

--- scriptt.js
+++ scriptt.js
@@ -36,3 +36,37 @@ document.addEventListener('DOMContentLoaded', function () {
         });
     });
 });
+
+document.addEventListener('DOMContentLoaded', function () {
+    const themeToggleBtn = document.getElementById('theme-toggle');
+    const body = document.body;
+
+    // Check for saved theme preference in localStorage
+    const savedTheme = localStorage.getItem('theme');
+    if (savedTheme) {
+        body.classList.add(savedTheme);
+    }
+
+    themeToggleBtn.addEventListener('click', function () {
+        body.classList.toggle('dark-theme');
+        
+        // Save the user's theme preference to localStorage
+        if (body.classList.contains('dark-theme')) {
+            localStorage.setItem('theme', 'dark-theme');
+        } else {
+            localStorage.removeItem('theme');
+        }
+    });
+});
+
+
+AOS.init({
+    offset: 120, // offset (in px) from the original trigger point
+    delay: 0, // values from 0 to 3000, with step 50ms
+    duration: 700, // values from 0 to 3000, with step 50ms
+    easing: 'ease', // default easing for AOS animations
+    once: true, // whether animation should happen only once - while scrolling down
+    mirror: false, // whether elements should animate out while scrolling past them
+    anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
+});
+
